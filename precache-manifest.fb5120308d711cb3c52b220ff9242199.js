df3ea71c4f147dd006c4ba0f38473b816f3cd9c7
Fernando Crozetta <fernando@czetta.com>
2019-04-22 04:39:40 -0300

    Updates

---
+++ precache-manifest.fb5120308d711cb3c52b220ff9242199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a58930ad6465a2c035",
+    "url": "/react-monopoly/static/js/runtime~main.1781a95b.js"
+  },
+  {
+    "revision": "329b6d75cbc121ccd6cb",
+    "url": "/react-monopoly/static/js/main.b7902917.chunk.js"
+  },
+  {
+    "revision": "2928a8a98bc00a7c400a",
+    "url": "/react-monopoly/static/js/2.9fb8ac8b.chunk.js"
+  },
+  {
+    "revision": "329b6d75cbc121ccd6cb",
+    "url": "/react-monopoly/static/css/main.d0b2afdd.chunk.css"
+  },
+  {
+    "revision": "2928a8a98bc00a7c400a",
+    "url": "/react-monopoly/static/css/2.5a9685bf.chunk.css"
+  },
+  {
+    "revision": "1744f1d24b64992c304c66921bfcb15c",
+    "url": "/react-monopoly/index.html"
+  }
+];
